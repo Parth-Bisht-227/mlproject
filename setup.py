8f8be3184fd766dafff53978754408bcc6c4d62a
Parth Bisht <bishtparth227@gmail.com>
2025-10-01 20:15:09 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if  HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+        return requirements    
+    
+
+
+setup( 
+    name = "mlproject", #metadata info about entire project
+    version = "0.0.1",
+    author = "Parth",
+    author_email = "bishtparth227@gmail.com",
+    packages = find_packages(where = "src"), # to look in src/
+    package_dir = {"":"src"}, # maps root to src/
+    install_requires = get_requirements('requirements.txt')
+)
